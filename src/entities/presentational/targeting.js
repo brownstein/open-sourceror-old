1c89803eba2b9789db2b7a9cb7b4a4464203923c
Robert Brownstein <robert.brownstein@gmail.com>
2020-06-03 21:57:27 -0400

    get target system working

--- src/entities/presentational/targeting.js
+++ src/entities/presentational/targeting.js
@@ -37,17 +37,79 @@ export class TargetingReticle {
       this.blockOutlineGeom,
       this.blockOutlineMaterial
     );
-    
-    this.blockOutlineMesh.position.z = 30;
+
     this.blockOutlineMesh.scale.multiplyScalar(8);
+
+    this.lineConnectionGeom = new Geometry();
+    this.lineConnectionGeom.vertices.push(new Vector3(0, 0, 0));
+    this.lineConnectionGeom.vertices.push(new Vector3(0, 0, 0));
+    this.lineConnectionGeom.vertices.push(new Vector3(0, 0, 0));
+    this.lineConnectionGeom.vertices.push(new Vector3(0, 0, 0));
+    this.lineConnectionGeom.faces.push(new Face3(0, 1, 2));
+    this.lineConnectionGeom.faces.push(new Face3(0, 2, 3));
+
+    this.lineConnectionMaterial = new MeshBasicMaterial({
+      side: DoubleSide,
+      transparent: true,
+      opacity: 0.5,
+      color: new Color(
+        0.5 + 0.5 * Math.random(),
+        0.5 + 0.5 * Math.random(),
+        0.5 + 0.5 * Math.random()
+      )
+    });
+
+    this.lineConnectionMesh = new Mesh(
+      this.lineConnectionGeom,
+      this.lineConnectionMaterial
+    );
 
     this.mesh = new Object3D();
     this.mesh.add(this.blockOutlineMesh);
+    this.mesh.add(this.lineConnectionMesh);
+    this.mesh.position.z = 30;
   }
   syncMeshWithViewport(viewport) {
-    this.mesh.position.copy(viewport.mouseSceneCoordinates);
-    this.mesh.position.x = Math.floor(this.mesh.position.x / 16) * 16 + 8;
-    this.mesh.position.y = Math.floor(this.mesh.position.y / 16) * 16 + 8;
+    const { engine } = this;
+    this.blockOutlineMesh.position.copy(viewport.mouseSceneCoordinates);
+    this.blockOutlineMesh.position.x = Math
+      .floor(this.blockOutlineMesh.position.x / 16) * 16 + 8;
+    this.blockOutlineMesh.position.y = Math
+      .floor(this.blockOutlineMesh.position.y / 16) * 16 + 8;
+
+    const playerPosition = engine.controllingEntity.mesh.position;
+    const posDiff = viewport.mouseSceneCoordinates
+      .clone()
+      .sub(playerPosition);
+    const posDiffLength = posDiff
+      .length();
+    const posDiffTangent = posDiff
+      .clone()
+      .normalize();
+    const posDiffNormal = new Vector3(
+      -posDiffTangent.y,
+      posDiffTangent.x,
+      0
+    );
+
+    this.lineConnectionMesh.position.copy(playerPosition);
+    this.lineConnectionGeom.vertices[0]
+      .multiplyScalar(0)
+      .add(posDiffNormal);
+    this.lineConnectionGeom.vertices[1]
+      .multiplyScalar(0)
+      .sub(posDiffNormal);
+    this.lineConnectionGeom.vertices[2]
+      .multiplyScalar(0)
+      .add(posDiffTangent)
+      .multiplyScalar(posDiffLength)
+      .sub(posDiffNormal);
+    this.lineConnectionGeom.vertices[3]
+      .multiplyScalar(0)
+      .add(posDiffTangent)
+      .multiplyScalar(posDiffLength)
+      .add(posDiffNormal);
+    this.lineConnectionGeom.verticesNeedUpdate = true;
   }
   setVisible(visible) {
     this.mesh.visible = visible;
