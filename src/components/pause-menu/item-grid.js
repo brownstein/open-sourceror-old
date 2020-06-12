67c5b331860e44c78b43a6ea2a2d90cce1514f59
Robert Brownstein <robert.brownstein@gmail.com>
2020-06-12 16:47:22 -0400

    get inventory drag system working

--- src/components/pause-menu/item-grid.js
+++ src/components/pause-menu/item-grid.js
@@ -30,8 +30,9 @@ import "./item-grid.less";
 class ItemGrid extends Component {
   constructor(props) {
     super(props);
-    const { inventory } = props;
+    const { inventory, inventorySize } = props;
 
+    this.mounted = false;
     this.dpr = window.devicePixelRatio;
     this.iconSize = 40;
 
@@ -41,28 +42,29 @@ class ItemGrid extends Component {
     this.camera = null;
     this.renderer = null;
 
-    this.inventoryRenderingData = {};
+    this.tileCanvasesAndContexts = [];
+    this.tileSprites = [];
 
-    const {
-      inventorySprites,
-      inventorySpritePromises
-    } = this._mapInventoryToSprites(inventory);
-
-    inventorySprites.map((s, i) => {
-      const data = {
-        sprite: s,
+    for (let i = 0; i < inventorySize; i++) {
+      this.tileCanvasesAndContexts[i] = {
         canvas: null,
         context: null
       };
-      this.inventoryRenderingData[i] = data;
-    });
+      this.tileSprites[i] = null;
+    }
 
     this.state = {
-      inventorySprites
+      draggingFromSlot: null,
+      draggingToSlot: null,
+      dragInventory: null
     };
 
+    this._prepareFrame = this._prepareFrame.bind(this);
     this._renderFrame = this._renderFrame.bind(this);
-    Promise.all(inventorySpritePromises).then(this._renderFrame);
+
+    this._onDragStart = this._onDragStart.bind(this);
+    this._onDropHover = this._onDropHover.bind(this);
+    this._onDragFinish = this._onDragFinish.bind(this);
   }
   componentDidMount() {
 
@@ -72,7 +74,6 @@ class ItemGrid extends Component {
 
     // initialize three.js (including webgl context)
     this.scene = new Scene();
-
     this.renderer = new WebGLRenderer({
       canvas: this.webGLCanvas,
       alpha: true,
@@ -81,7 +82,6 @@ class ItemGrid extends Component {
     this.renderer.setClearAlpha(0);
     this.renderer.setPixelRatio(this.dpr);
     this.renderer.setSize(this.iconSize, this.iconSize);
-
     this.camera = new OrthographicCamera(
       -10, 10,
       -10, 10,
@@ -89,77 +89,138 @@ class ItemGrid extends Component {
     );
     this.camera.lookAt(new Vector3(0, 0, -1));
 
-    // initialize canvas 2d contexts
-    this.state.inventorySprites.map((s, i) => {
-      if (!s) {
-        return;
-      }
-      const data = this.inventoryRenderingData[i];
+    // initialize the onscreen canvases
+    for (let i = 0; i < this.tileCanvasesAndContexts.length; i++) {
+      const data = this.tileCanvasesAndContexts[i];
       data.canvas.width = 40 * this.dpr;
       data.canvas.height = 40 * this.dpr;
       data.context = data.canvas.getContext("2d");
       data.context.scale(this.dpr, this.dpr);
-    });
+    }
+
+    this.mounted = true;
+    this._prepareFrame();
+  }
+  componentDidUpdate() {
+    this._prepareFrame();
   }
   componentWillUnmount() {
+    this.mounted = false;
     this.renderer.dispose();
   }
-  _mapInventoryToSprites(inventory,) {
-    const inventorySprites = [];
-    const inventorySpritePromises = [];
+  _prepareFrame() {
+    if (!this.mounted) {
+      return;
+    }
+    const { inventory, inventorySize } = this.props;
+    const { dragInventory } = this.state;
 
-    inventory.forEach(itemName => {
-      if (itemName) {
-        const ItemClass = items[itemName];
-        if (!ItemClass) {
-          throw new Error("Missing item", itemName);
-        }
-        const itemSprite = ItemClass.getIcon();
-        inventorySprites.push(itemSprite);
-        inventorySpritePromises.push(itemSprite.readyPromise);
-      }
-      else {
-        inventorySprites.push(null);
-      }
-    });
+    const currentInventory = dragInventory || inventory;
 
-    return {
-      inventorySprites,
-      inventorySpritePromises
-    };
+    const promises = [];
+    for (let i = 0; i < inventorySize; i++) {
+      const { itemName, id } = currentInventory[i] || {};
+      if (!itemName) {
+        this.tileSprites[i] = null;
+        continue;
+      }
+      const ItemClass = items[itemName];
+      if (!ItemClass) {
+        throw new Error("Missing item", itemName);
+      }
+      const itemSprite = ItemClass.getIcon();
+      this.tileSprites[i] = itemSprite;
+      promises.push(itemSprite.readyPromise);
+    }
+    Promise.all(promises).then(this._renderFrame);
   }
   _renderFrame() {
-    this.state.inventorySprites.map((s, i) => {
-      if (!s) {
-        return;
+    if (!this.mounted) {
+      return;
+    }
+    for (let i = 0; i < this.tileSprites.length; i++) {
+      const tileData = this.tileCanvasesAndContexts[i];
+      const { canvas, context } = tileData;
+
+      // clear the 2D canvas no matter what
+      context.clearRect(0, 0, canvas.width, canvas.height);
+
+      // get the sprite
+      const sprite = this.tileSprites[i];
+      if (!sprite) {
+        continue;
       }
-      const data = this.inventoryRenderingData[i];
-      const { context } = data;
 
-      this.scene.add(data.sprite.mesh);
-      data.sprite.mesh.position.x = i * 16;
-      this.camera.position.x = i * 16;
+      // render the sprite into the scene and then immediately remove it
+      if (sprite.mesh) {
+        this.scene.add(sprite.mesh);
+        this.renderer.render(this.scene, this.camera);
+        this.scene.remove(sprite.mesh);
+      }
 
-      this.renderer.render(this.scene, this.camera);
-      const rendererContext = this.renderer.getContext();
+      // copy webgl canvas to inexpensive 2D canvas
       context.drawImage(this.webGLCanvas, 0, 0, this.iconSize, this.iconSize);
+    }
+  }
+  _onDragStart(draggingFromSlot) {
+    this.setState({
+      draggingFromSlot: draggingFromSlot,
+      dragInventory: [...this.props.inventory]
     });
   }
-  _moveItem() {
-    return false;
+  _onDropHover(draggingToSlot) {
+    const { inventory } = this.props;
+    const { draggingFromSlot, draggingToSlot: current } = this.state;
+    if (current === draggingToSlot) {
+      return;
+    }
+
+    const dragInventory = [...inventory];
+    const a = dragInventory[draggingFromSlot] || null;
+    const b = dragInventory[draggingToSlot] || null;
+    dragInventory[draggingFromSlot] = b;
+    dragInventory[draggingToSlot] = a;
+
+    this.setState({
+      draggingToSlot,
+      dragInventory
+    });
+  }
+  _onDragFinish(onTarget) {
+    const { moveItem } = this.props;
+    const { draggingFromSlot, draggingToSlot } = this.state;
+
+    if (onTarget && draggingFromSlot !== draggingToSlot) {
+      moveItem(draggingFromSlot, draggingToSlot);
+    }
+
+    this.setState({
+      draggingFromSlot: null,
+      draggingToSlot: null,
+      dragInventory: null
+    });
   }
   render() {
     const { inventory, inventorySize } = this.props;
-    const { inventoryRenderingData } = this;
+    const {
+      tileCanvasesAndContexts,
+      _onDragStart,
+      _onDropHover,
+      _onDragFinish
+    } = this;
 
     const tiles = [];
-
     for (let i = 0; i < inventorySize; i++) {
-      const index = i;
-      const data = inventoryRenderingData[index] || {};
+      const item = inventory[i] || { itemName: null };
+      const tileData = tileCanvasesAndContexts[i];
       tiles.push(
-        <ItemTile key={index} id={index}>
-          <canvas ref={el => data.canvas = el}/>
+        <ItemTile key={i} index={i} {...{
+          onDragStart: _onDragStart,
+          onDropHover: _onDropHover,
+          onDragFinish: _onDragFinish
+        }}>
+          <canvas ref={el => tileData.canvas = el }/>
+          <div className="itemname">{item.itemName}</div>
         </ItemTile>
       );
     }
@@ -172,13 +233,47 @@ class ItemGrid extends Component {
   }
 }
 
-function ItemTile ({ id, children }) {
+function ItemTile ({
+  index,
+  children,
+  onDragStart,
+  onDropHover,
+  onDragFinish
+}) {
   const ref = useRef(null);
   const [,drop] = useDrop({
     accept: "TILE",
+    hover: (item, monitor) => {
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      const hBBox = ref.current.getBoundingClientRect();
+      const hCenter = {
+        x: hBBox.left + hBBox.width / 2,
+        y: hBBox.top + hBBox.height / 2
+      };
+      const clientOffset = monitor.getClientOffset();
+      if (
+        Math.abs(clientOffset.x - hCenter.x) < 20 &&
+        Math.abs(clientOffset.y - hCenter.y) < 20
+      ) {
+        onDropHover(hoverIndex);
+      }
+    }
   });
   const [{ isDragging }, drag] = useDrag({
-    item: { id, type: "TILE" }
+    item: { index, id: index, type: "TILE" },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging()
+    }),
+    begin: (monitor) => {
+      onDragStart(index);
+    },
+    end: (dropResult, monitor) => {
+      onDragFinish(monitor.didDrop());
+    }
   });
 
   const style = {
@@ -195,9 +290,18 @@ function ItemTile ({ id, children }) {
 function mapStateToProps(state) {
   const { inventory } = state;
   return {
-    inventory: inventory.inventory,
+    inventory: inventory.inventory.map((itemName, id) => ({ itemName, id })),
     inventorySize: inventory.inventorySize
   };
 }
 
-export default connect(mapStateToProps)(ItemGrid);
+function mapDispatchToProps(dispatch) {
+  return {
+    moveItem: (fromSlot, toSlot) => dispatch(moveItemInInventory({
+      fromSlot,
+      toSlot
+    }))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemGrid);
