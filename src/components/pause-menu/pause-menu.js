cabc783f4a144f5f03c6dc562bf8134fc8526cc6
Robert Brownstein <robert.brownstein@gmail.com>
2020-06-13 18:54:37 -0400

    reinstate pause menu

--- src/components/pause-menu/pause-menu.js
+++ src/components/pause-menu/pause-menu.js
@@ -22,7 +22,7 @@ export default function PauseMenu () {
     return () => window.removeEventListener("keyup", unPauseListener);
   }, [running]);
 
-  if (true || running) {
+  if (running) {
     return null;
   }
   return (
