1669cb8088760df32792a98037479ebe5c5eeef7
Robert Brownstein <robert.brownstein@gmail.com>
2020-06-13 23:06:18 -0400

    get sdf working

--- src/entities/background/background-text.js
+++ src/entities/background/background-text.js
@@ -5,10 +5,18 @@ import {
   Mesh,
   MeshBasicMaterial,
   Object3D,
-  ShapeBufferGeometry
+  RawShaderMaterial,
+  ShapeBufferGeometry,
+  TextureLoader
 } from "three";
+import createTextGeometry from "three-bmfont-text";
+import loadBMFont from "load-bmfont/browser";
+import SDFShader from 'three-bmfont-text/shaders/sdf';
 
 import { castToVector3 } from "src/p2-utils/vec2-utils";
+
+import robotoFnt from "src/fonts/sdf/roboto-regular.fnt";
+import robotoPng from "src/fonts/sdf/roboto-regular.png";
 
 import robotoRegular from "src/fonts/json/roboto-regular.json";
 
@@ -18,32 +26,51 @@ export class BackgroundText {
     const text = props.text || "[]";
     const textSize = props.size || 12;
 
-    this.fontJson = robotoRegular;
-    this.fontLoader = new FontLoader();
-    this.font = this.fontLoader.parse(this.fontJson);
-
-    this.shape = this.font.generateShapes(text, 12);
-    this.geometry = new ShapeBufferGeometry(this.shape, 4);
-    this.material1 = new MeshBasicMaterial({
-      color: new Color("#ffffff"),
-      side: DoubleSide
-    });
-    this.material2 = new MeshBasicMaterial({
-      color: new Color("#444444"),
-      side: DoubleSide
-    });
+    this.text = text;
+    this.textSize = textSize;
+    this.textureLoader = new TextureLoader();
 
     this.mesh = new Object3D();
-    const mainMesh = new Mesh(this.geometry, this.material1);
-    const secondMesh = new Mesh(this.geometry, this.material2);
-    this.mesh.add(mainMesh);
-    this.mesh.add(secondMesh);
-    mainMesh.rotation.x = Math.PI;
-    secondMesh.rotation.x = Math.PI;
-    secondMesh.position.x = 1;
-    secondMesh.position.y = 1;
-    secondMesh.position.z = -1;
-
     this.mesh.position.copy(position);
+    this.mesh.position.z = -10;
+    this.readyPromise = this._init();
+  }
+  async _init() {
+    this.texture = await this.textureLoader.loadAsync(robotoPng);
+    this.font = await new Promise((resolve, reject) =>
+      loadBMFont(robotoFnt, (err, fnt) => (err ? reject(err) : resolve(fnt)))
+    );
+    this.geometry = createTextGeometry({
+      align: "center",
+      font: this.font
+    });
+    this.geometry.update(this.text);
+    const material = new RawShaderMaterial(SDFShader({
+      map: this.texture,
+      transparent: true,
+      color: 0xffffff,
+      side: DoubleSide
+    }));
+    const outlineMaterial = new RawShaderMaterial(SDFShader({
+      map: this.texture,
+      transparent: true,
+      opacity: 0.25,
+      color: 0x444444,
+      side: DoubleSide
+    }));
+
+    const mesh = new Mesh(this.geometry, material);
+    mesh.scale.multiplyScalar(0.5);
+    this.mesh.add(mesh);
+
+    // add outlines for visibility
+    for (let i = 0; i < 8; i++) {
+      const om = new Mesh(this.geometry, outlineMaterial);
+      om.position.x += Math.cos(i * Math.PI / 4);
+      om.position.y += Math.sin(i * Math.PI / 4);
+      om.position.z = -1;
+      om.scale.multiplyScalar(0.5);
+      this.mesh.add(om);
+    }
   }
 }
